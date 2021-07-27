a57bae3bb9258f2b2595a9aa2b8ef28f08567708
Maxim Mitin <maximus405@gmail.com>
2021-07-27 19:37:05 +0300

    Initial Commit

---
+++ HW.2_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2_7
+//
+//  Created by Maxim Mitin on 27.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
